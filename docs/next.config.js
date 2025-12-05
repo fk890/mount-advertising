b4e3abed89a4b7e15745031683b74f73bf1a423a
fk890 <fk890583@gmail.com>
2025-12-06 00:55:15 +0530

    fix: downgrade nextra to 2.x for compatibility

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,14 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-
-module.exports = {
-  ...withNextra({
-    images: {
-      domains: ["user-images.githubusercontent.com"],
-    },
-    basePath: '/docs',
-  }),
-}
+module.exports = withNextra();
