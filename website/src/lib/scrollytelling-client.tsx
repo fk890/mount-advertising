69773bddc319ed964fb41e576c12db9c35c7ad17
fk890 <fk890583@gmail.com>
2026-01-30 00:42:00 +0530

    Fix scrollytelling re-exports for type safety during CI builds

--- website/src/lib/scrollytelling-client.tsx
+++ website/src/lib/scrollytelling-client.tsx
@@ -1,15 +1,4 @@
 "use client";
 
-// Re-export from the actual scrollytelling package which is built locally
-export {
-	Root,
-	Animation,
-	Parallax,
-	Pin,
-	RegisterGsapPlugins,
-	Waypoint,
-	Stagger,
-	ImageSequenceCanvas,
-	useScrollytelling,
-	useScrollToLabel,
-} from "@bsmnt/scrollytelling";
+// Re-export everything from the actual scrollytelling package which is built locally
+export * from "@bsmnt/scrollytelling";
