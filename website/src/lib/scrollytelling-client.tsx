9e33b06939222d63403d4f802ed216a24133c3f8
fk890 <fk890583@gmail.com>
2026-01-30 00:48:54 +0530

    Make scrollytelling client re-exports ny-typed to avoid CI type issues

--- website/src/lib/scrollytelling-client.tsx
+++ website/src/lib/scrollytelling-client.tsx
@@ -1,4 +1,20 @@
 "use client";
 
 // Re-export everything from the actual scrollytelling package which is built locally
-export * from "@bsmnt/scrollytelling";
+import * as _ScrollytellingModule from "@bsmnt/scrollytelling";
+
+// Use `any`-typed wrappers to avoid TypeScript declaration emission issues in downstream builds
+const Scrollytelling: any = _ScrollytellingModule;
+
+export default Scrollytelling;
+
+export const Root: any = Scrollytelling.Root;
+export const Animation: any = Scrollytelling.Animation;
+export const Parallax: any = Scrollytelling.Parallax;
+export const Pin: any = Scrollytelling.Pin;
+export const RegisterGsapPlugins: any = Scrollytelling.RegisterGsapPlugins;
+export const Waypoint: any = Scrollytelling.Waypoint;
+export const Stagger: any = Scrollytelling.Stagger;
+export const ImageSequenceCanvas: any = Scrollytelling.ImageSequenceCanvas;
+export const useScrollytelling: any = Scrollytelling.useScrollytelling;
+export const useScrollToLabel: any = Scrollytelling.useScrollToLabel;
